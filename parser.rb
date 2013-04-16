d332b279ff8bd3ea2e9ad687aa46f141da985a45
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2013-04-16 00:24:34 -0300

    Remove empty tags

--- parser.rb
+++ parser.rb
@@ -3,18 +3,6 @@ require "open-uri"
 require "nokogiri"
 require "cgi"
 
-# TODO: remove empty tags
-# def is_blank?(node)
-#   node.text? && node.content.strip == ''
-# end
-# def all_children_are_blank?(node)
-#   node.children.all?{ |child| is_blank?(child) }
-# end
-# doc.xpath("//*").find_all{ |p| all_children_are_blank?(p) }.each do |p|
-#   puts p
-#   p.remove
-# end
-
 output = "out.html"
 url = "https://docs.google.com/document/d/1fUye_omrE5jgMnljL9ZuvK-BJUhyRM6dCQ8KTtunC70"
 
@@ -25,7 +13,7 @@ default_style = <<-HTML
       padding: 0 10px 5px 10px;
     }
     .docs-content { /* margin-left: 280px; float:left; padding-left: 270px; width: 700px; */ }
-    .docs-content img { border-radius: 2px; margin: 10px; box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.3); max-width: 600px; }
+    .docs-content img { border-radius: 2px; margin: 20px 10px; box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.3); max-width: 600px; }
     .disclaimer { color: red; }
     #menu.affix { top: 20px; }
     a.anchor { margin-top: 20px; }
@@ -54,6 +42,13 @@ end
 
 # Remove the title
 doc.xpath('//*[contains(@class, "title")]').remove
+
+# Remove empty tags
+def is_blank?(node)
+  node.content.strip == '' and node.children.length == 0
+end
+doc.css("span").select{ |p| is_blank?(p) }.each{ |node| node.remove }
+doc.css("p").select{ |p| is_blank?(p) }.each{ |node| node.remove }
 
 # The content we want
 content = doc.xpath("//*[preceding-sibling::style]")
